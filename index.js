21701917c932e6315e2824190b8c9009bac807e6
Musa-aza <Musa.azarkan@gmail.com>
2024-08-31 17:28:35 +0100

    commit init

---
+++ index.js
@@ -0,0 +1,17 @@
+//This is my first JavaScript code!
+
+// Cannot be a reserved keyword
+// Should be a meaningfull word
+// Cannot start with a number (1name)
+// Cannot conain space or hyphen (-)
+// Are chase-senetive
+
+let name = 'Musa'; // String literal
+let age = 19; // Number literal
+let isApproved = true; // Boolean literal (can only be 'true' or 'false')
+let firstName = undefined;
+let selectedColor = null;
+
+let intrestRate = 0.3;
+intrestRate = 1;
+console.log(intrestRate);
